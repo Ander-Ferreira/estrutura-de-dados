8ace20ef2c6334191b8d547c7acbeed8e98043d8
Anderson Ferreira <andersonadm1314@gmail.com>
2024-03-05 19:50:37 -0300

    Exercicios Aula 03

---
+++ Aula 03/exercicio-05.js
@@ -0,0 +1,15 @@
+class aluno {
+    constructor(nome, idade, peso){
+    this.nome = nome;
+    this.idade = idade;
+    this.peso = peso;
+    }
+
+  }
+
+  aluno.nome = 'Anderson'
+  console.log(aluno.nome)
+  aluno.idade = 95
+  console.log(aluno.idade)
+  aluno.peso = 200
+  console.log(aluno.peso)
