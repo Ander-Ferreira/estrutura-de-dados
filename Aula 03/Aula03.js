650bad7174ced509d35b0a180a5c2ad7e90486c0
Ander-Ferreira <andersonadm1314@gmail.com>
2024-03-06 21:54:56 -0300

    Terceira Aula

---
+++ Aula 03/Aula03.js
@@ -0,0 +1,123 @@
+var num = 0
+/*
+if(num === 1){
+ console.log('num é igual a 1')
+}
+else{
+ console.log('num é igual a um, e o valor de num: ' + num)
+} */
+
+/*
+if ( num === 1 ) {
+    num--;
+}
+
+else{
+    num++
+}
+
+console.log(num)
+*/
+
+//(num === 1) ? num -- : num++;
+
+var mes = 5;
+/*
+if(mes === 1) {
+    console.log('Janeiro')
+}
+
+else if(mes === 2) {
+    console.log('Fevereiro')
+}
+else if(mes === 3){
+    console.log('Março')
+}
+else if(mes ===4){
+    console.log('Abril')
+}
+else{
+    console.log('O mês não é Janeiro, é Fevereiro ou Março')
+}
+
+*/
+
+
+/*
+switch (mes) {
+    case 1:
+        console.log('Janeiro')
+        break
+
+    case 2:
+        console.log('Fevereuro')
+        break
+    
+    case 4:
+        console.log('Março')
+        break
+    
+    case 5:
+        console.log('Abril')
+        break
+
+    default:
+        console.log('O mês não é Janeiro, é Fevereiro ou Março')
+    }
+
+
+/*for(var i = 0; i < 10; i++) {
+    console.log(i)
+}
+*/
+
+/*
+
+while(i < 10){
+    i++
+    console.log(i)
+
+}
+
+*/
+
+/*
+
+var i = 0
+
+do{
+    console.log(i)
+    i++
+} while(i < 10)
+
+*/
+
+function faleOla(){
+    console.log('Olá')
+}
+
+function output(text){
+    console.log(text)
+}
+
+function sum(num1, num2){
+
+}
+
+var result = sum(2, 3);
+output(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
