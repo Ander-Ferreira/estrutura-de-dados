278db72f3a294aad3545e1609baf3a5bd11c6b31
Anderson Ferreira <andersonadm1314@gmail.com>
2024-03-28 16:06:09 -0300

    Correção do exercício 1

--- lista-exercicios-2/exercicio1.js
+++ lista-exercicios-2/exercicio1.js
@@ -1,74 +1,31 @@
-/*
+    /*
 01. Crie uma função que recebe um array de 2 números e coloque eles em ordem crescente. Não
 use o método sort.
 */
 
 
-class Stack {
-    constructor(){
-    this.items = []
+let array = []
 
-    }
-    push(element){
-        //Adiciona um elemento
-        this.items.push(element)
-    }
-    pop(){
-        //Retira um elemento do topo da pilha
-        return this.items.pop()
-
-    }
-    peek(){
-        //devolve o elemento que está no topo da pilha
-        return this.items[this.items.lenght - 1]
-
-
-    }
-    isEmpty(){
-        //Informa se as pilhas estão de vazias ou não
-        return this.items.length === 0
-    }
-    clear(){
-        //Limpa a pilha
-        this.items = []
-
-    }
-    size(){
-        //informa o tamnho da pilha
-        return this.items.length
-    }
-    print(){
-        //imprime a pilha no console
-        console.log(this.items.toString())
-
-    }
-}
-
-
-function arrayVazio (array, numeroA, numeroB){
-    array = []
+function arrayVazio (numA, numB){
+    array.push(numA)
+    array.push(numB)
     
-   /*
-    var numA = 1
-    var numB = 2
     
-    array.push(numA, numB)
-  */
-  
-    numeroA.push(numeroA)
-    numberoB.push(numberoB)
+
+    array.push(numA)
+    array.push(numB)
     
 
     
 
 
-    if(numeroA > numeroB){
+    if(numA > numB){
         console.log(`${numA} é maior do que ${numB}`)
 
     }
 
-    if(numeroA < numeroB){
-        console.log(`${numB} é menor do que ${numA}`)
+    if(numA < numB){
+        console.log(`${numB} é maior do que ${numA}`)
     }
     
 
