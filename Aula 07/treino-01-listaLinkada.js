8710ebe164fd76dcffd25e305ad6498d861e5e9b
Anderson Ferreira <andersonadm1314@gmail.com>
2024-04-29 17:17:57 -0300

    atualizando lista de treino com novas funções isEmpty() e indexOf()

--- Aula 07/treino-01-listaLinkada.js
+++ Aula 07/treino-01-listaLinkada.js
@@ -1,29 +1,94 @@
-function listaLinkada(){
+function listaLinkada() {
     let length = 0
-    let head = null
+    let inicio = null
 
-    let Node = function(element){
+    let Node = function (element) {
         this.element = element
         this.next = null
+
+
+    }
+    this.add = function (element) {
+        let node = new Node(element)
+        if (inicio === null) {
+            inicio = node
+        }
+
+        else {
+            let atual = inicio
+            while (atual.next) {
+                atual = atual.next
+            }
+            atual.next = node
+
+        }
+        length++
     }
 
-    this.add = function(element){
-        let node = new Node(element)
+    /*this.remove = function(element){
+        let currentNode = head
+        let previousNode
 
-        if(head === null){
-            head = node
+        if(currentNode.element === element){
+            head = currentNode.next
         }
         else{
-            let currentNode = head
-
-            while(currentNode.next){
+            while(currentNode.element !== element){
+                
+                previousNode = currentNode
                 currentNode = currentNode.next
-            }
-            currentNode.next = node
-        }
+                previousNode.next = currentNode.next 
 
-        length++
+
+
+            }
+            length --
+        }
+    }
+    */
+
+   this.remove = function(element){
+    let atual = inicio
+    let anterior
+    
+    if(atual.element === element){
+        inicio = atual.next
+    }
+    else{
+        while(atual.element !== element){
+            anterior = atual
+            atual = atual.next
+        }
+        anterior.next = atual.next
 
     }
+    length--
+   }
+
+   this.isEmpty = function(){
+    return length === 0
+   }
+
+   this.indexOf = function (element){
+     let atual = inicio
+
+     let indice = -1
+
+     while(atual){
+        indice++
+        if(atual.element === element){
+            return indice
+        }
+        atual = atual.next
+        
+     }
+
+     return -1
+   }
+   
+
+   
 }
+
+
 
